2c7e2b3279e89086c2c3dc39b47f9c7e41a7c1e4
leocasarsa <leonardocazevedo@gmail.com>
2017-05-04 10:26:27 +0200

    Add tests for layers with single neuron (not implemented yet).

--- test_multilayer_perceptron.py
+++ test_multilayer_perceptron.py
@@ -7,18 +7,19 @@ def identity(x):
 def test_single_input_neuron():
     '''Outputs the identity of the input value.'''
     neuron = Neuron(neuron_type='input')
-
     for x in [0, -10, np.pi]:
         assert neuron.compute_output(x) == x
+
+    layer = Layer(layer_type='input')
+    for x in [0, -10, np.pi]:
+        assert layer.compute_output(x) == [x]
 
 def test_single_output_neuron():
     '''
     Outputs the linear combination of the input
     values, weighted by the connection weights.'''
+    bias = 1
     connection_weights = [1, -2, 1./3]
-    neuron = Neuron(neuron_type='output', bias=1,
-                connection_weights=connection_weights)
-
     input_values = [[-1, -.5, -3],
                     [2, 1, 0],
                     [2, 2, 9]]
@@ -28,19 +29,23 @@ def test_single_output_neuron():
     # 2 = 1 + 1*2 + -2*2 + 1./3*9
     output_values = [0, 1, 2]
 
+    neuron = Neuron(neuron_type='output', bias=bias,
+                connection_weights=connection_weights)
     for x, y in zip(input_values, output_values):
         assert neuron.compute_output(x) == y
+
+    layer = Layer(layer_type='output', layer_biases=[bias],
+                  layer_weights=[connection_weights])
+    for x, y in zip(input_values, output_values):
+        assert layer.compute_output(x) == [y]
 
 def test_single_hidden_neuron_step():
     '''
     Outputs the linear combination of the input
     values, weighted by the connection weights,
     and passed through the activation function.'''
-
+    bias = 0
     connection_weights = [1, -2, 1./3]
-    neuron = Neuron(neuron_type='hidden', bias=0,
-                connection_weights=connection_weights)
-
     input_values = [[-1, -.5, -3],
                     [2, 1, 0],
                     [2, 2, 9]]
@@ -50,8 +55,16 @@ def test_single_hidden_neuron_step():
     # 1 = step(0 + 1*2 + -2*2 + 1./3*9) = step(1)
     output_values = [-1, 1, 1]
 
+    neuron = Neuron(neuron_type='hidden', bias=bias,
+                connection_weights=connection_weights)
     for x, y in zip(input_values, output_values):
         assert neuron.compute_output(x) == y
+
+    layer = Layer(layer_type='hidden', layer_biases=[bias],
+                  layer_weights=[connection_weights])
+    for x, y in zip(input_values, output_values):
+        assert layer.compute_output(x) == [y]
+
 
 def test_input_layer():
     '''
